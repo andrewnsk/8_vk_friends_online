52aedf5b1f0cc66aab9e2b3e71c7ab0e24da3225
Andrew <andrewnorilsk@gmail.com>
2017-11-15 15:58:09 +0300

    Added main functional

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,16 @@
+import os
 import vk
+import getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = os.environ.get('VFO_APP_ID')
 
 
 def get_user_login():
-    pass
+    return input('Enter user login / e-mail / phone number > ')
 
 
 def get_user_password():
-    pass
+    return getpass.getpass(prompt='Password: (will not be echoed) > ', stream=None)
 
 
 def get_online_friends(login, password):
@@ -19,11 +20,18 @@ def get_online_friends(login, password):
         user_password=password,
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    all_friends = api.friends.get(fields='online')
+    return [online_friend for online_friend in all_friends if online_friend['online'] == 1]
 
 
 def output_friends_to_console(friends_online):
-    pass
+    if len(friends_online) != 0:
+        print('Friends online: ')
+        for friend in friends_online:
+            print(friend['first_name'], friend['last_name'])
+        print('Total friends online: ', len(friends_online))
+    else:
+        print('No friends online')
 
 if __name__ == '__main__':
     login = get_user_login()
